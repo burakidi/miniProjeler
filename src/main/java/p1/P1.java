bead225d41d672a7b571b0f30ccd828cce276c88
Burak İdi <burak.idi@yandex.com>
2023-06-24 15:25:17 +0300

    yeni2

--- src/main/java/p1/P1.java
+++ src/main/java/p1/P1.java
@@ -3,11 +3,6 @@ package p1;
 public class P1 {
     public static void main(String[] args) {
         System.out.println("deniyorum v1");
-
-        System.out.println("v3");
-
-//yeni denemeler v4
-
-//denemeler devam v5
+//v2 yeni şeyler
     }
 }
